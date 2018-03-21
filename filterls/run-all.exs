85f6e65d1c0dac82f34c1f28d979012351284807
Lambros Petrou <lambrospetrou@gmail.com>
2018-03-21 22:36:15 +0000

    Fix the run-all script to read all data files

--- filterls/run-all.exs
+++ filterls/run-all.exs
@@ -1,19 +1,19 @@
 defmodule FilterlsRunner do
 
   def run() do
-    [#{"filter-go", "filter-go"},
+    [{"filter-go", "filter-go"},
      {"filter.go", "filter.go"},
-     #{"filter-ex 0", "filter-ex-pat"},
-     #{"filter-ex 1", "filter-ex-split"},
-     #{"filter.rkt", "filter.rkt"},
+     {"filter-ex 0", "filter-ex-pat"},
+     {"filter-ex 1", "filter-ex-split"},
+     {"filter.rkt", "filter.rkt"},
      {"filter.py", "filter.py"},
      {"filter.rb", "filter.rb"}]
     |> Enum.each(&runBinary/1)
   end
 
   defp runBinary({bin, bin_output}) do
-    files = ["data.txt", "dataMM.txt"]
-    #files = String.Chars.to_string(:os.cmd('ls ./test-files')) |> String.split()
+    #files = ["data.txt", "dataMM.txt"]
+    files = String.Chars.to_string(:os.cmd('ls ./test-files')) |> String.split()
     files
     |> Enum.each(fn datafile ->
       IO.puts("Processing #{bin_output} - #{datafile}")
