d2dc718825a1bdfdc4da1b77616bbafd80e29897
Lambros Petrou <lambrospetrou@gmail.com>
2018-07-02 00:55:34 +0100

    Add another Clojure filterer, but 3x slower

--- filterls/filter.clj
+++ filterls/filter.clj
@@ -2,17 +2,24 @@
 
 (require '[clojure.string :as str])
 
+(defn isnumeric [s] 
+  (and
+    (not (empty? s))
+    (every? #(Character/isDigit %) s)))
+
+(defn filterer2 [line]
+  (def col2 (get (str/split line #" " 3) 1))
+  (if (isnumeric col2)
+    (> (Integer/parseInt (str/trim col2)) 10)
+    false))
+
 (defn filterer [line]
   (try 
     (> (Integer/parseInt (str/trim (get (str/split line #" " 3) 1))) 10)
     (catch Exception e false)))
 
 (defn process [in out]
-  (with-open 
-    [
-      bufout (java.io.BufferedWriter. out)
-      bufin (java.io.BufferedReader. in)
-    ]
+  (with-open [bufout (java.io.BufferedWriter. out) bufin (java.io.BufferedReader. in)]
     (doseq
       [l (line-seq bufin)]
       (if (filterer l) (do (.write bufout l) (.newLine bufout))))))
@@ -22,7 +29,5 @@
 
 (defn process-stdio [] (process *in* *out*))
 
-
-(time (process-file "test-files/data.txt" "output/clojure.out"))
-
+; (time (process-file "test-files/data.txt" "output/clojure.out"))
 ;(process (java.io.StringReader. (slurp "test-files/data.txt")))
