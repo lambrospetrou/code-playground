847aa65845eb0d81ce664bb3055842ba9668684c
Lambros Petrou <lambrospetrou@gmail.com>
2019-02-05 23:00:50 +0000

    Solve filterjs with CLJS and Planck

--- cljs-scripting/planck/hello.cljs
+++ cljs-scripting/planck/hello.cljs
@@ -1,15 +1,32 @@
 #!/usr/bin/env plk
 
 ; http://planck-repl.org/scripts.html
+; http://planck-repl.org/sdk.html
 
 (ns example.echo
-  (:require [planck.core :as core]))
+  (:require [planck.core :as core]
+            [goog.string :as gstr]
+            [clojure.string :as str]))
 
-(doseq [l (core/line-seq core/*in*)]
-  (println l))
+; (doseq [l (core/line-seq core/*in*)]
+;   (println l))
 
-(doseq [arg *command-line-args*]
-  (println arg))
+; (doseq [arg *command-line-args*]
+;   (println arg))
 
-(doseq [file (core/file-seq "/tmp")]
-  (println (:path file)))
+; (doseq [file (core/file-seq "/tmp")]
+;   (println (:path file)))
+
+(defn filterls-lines
+  ([lines] (filterls-lines lines *out*))
+  ([lines out]
+   (doseq [l lines]
+     (let [parts (str/split l #" " 3)
+           second-col (get parts 1)
+           num (if (gstr/isNumeric second-col) (gstr/parseInt second-col) nil)]
+       (if (> num 10) (do (-write *out* l) (newline)))))))
+
+(defn filterls [] (filterls-lines (core/line-seq core/*in*)))
+
+(time (example.echo/filterls-lines (planck.core/line-seq (planck.io/reader "../../filterls/test-files/data.txt")) (planck.io/writer "./results.filterls.out")))
+(time (example.echo/filterls-lines (planck.core/line-seq (planck.io/reader "../../filterls/test-files/dataMM.txt")) (planck.io/writer "./results.filterls.out")))
