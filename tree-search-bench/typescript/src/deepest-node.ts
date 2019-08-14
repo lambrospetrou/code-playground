e8c26f27a89c030677f910fa2dd29904f5876bb7
Lambros Petrou <lambrospetrou@gmail.com>
2019-08-14 21:21:41 +0100

    Add Google TypeScript Style (gts)

--- tree-search-bench/typescript/src/deepest-node.ts
+++ tree-search-bench/typescript/src/deepest-node.ts
@@ -2,50 +2,54 @@
 
 // I use `node` above instead of `ts-node` because this will be compiled to JS before running.
 
-import { readFileSync } from "fs";
+import { readFileSync } from 'fs';
 
 /**
  * Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
-
-      a
-    / \
-   b  c
-  /
-  d
-
-*/
+ *
+ *     a
+ *   / \
+ *  b  c
+ * /
+ * d
+ *
+ */
 
 interface Node {
-  left?: Node,
-  right?: Node,
-  val: any
+  left?: Node;
+  right?: Node;
+  // tslint:disable-next-line: no-any
+  val: any;
 }
 
-export const deepestNode = (root: Node) : Node => {
-  const do_deep_rec = (n: Node | undefined, depth: number) : [Node | undefined, number] => {
+export const deepestNode = (root: Node): Node => {
+  const doDeepRec = (
+    n: Node | undefined,
+    depth: number
+  ): [Node | undefined, number] => {
     if (!n) {
-      return [undefined, 0]
+      return [undefined, 0];
     }
     if (!n.left && !n.right) {
       return [n, depth];
     }
 
-    const [left, dleft] = do_deep_rec(n.left, depth + 1);
-    const [right, dright] = do_deep_rec(n.right, depth + 1);
+    const [left, dleft] = doDeepRec(n.left, depth + 1);
+    const [right, dright] = doDeepRec(n.right, depth + 1);
 
     if (dleft > dright) {
       return [left, dleft];
     }
     return [right, dright];
-  }
-  return do_deep_rec(root, 1)[0] as Node
-}
+  };
+  return doDeepRec(root, 1)[0] as Node;
+};
 
 const inputPath = process.argv[2];
 const tree = JSON.parse(String(readFileSync(inputPath)));
 
-console.time("deepestNode");
+console.time('deepestNode');
 const result = deepestNode(tree);
-console.timeEnd("deepestNode");
+console.timeEnd('deepestNode');
 
 console.error(JSON.stringify(result));
